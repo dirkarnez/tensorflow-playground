e618cfeb8c53d43c6b81648140b02aabc92ee064
dirkarnez <smalldirkalex@gmail.com>
2022-12-03 21:19:46 +0800

    - add sample code

--- local-build-msvc2019.cmd
+++ local-build-msvc2019.cmd
@@ -11,6 +11,8 @@ SET PATH=^
 %DOWNLOAD_DIR%\PortableGit\bin;^
 %DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;
 
+@REM -Dtensorflow_ROOT="%DOWNLOAD_DIR_LINUX%/libtensorflow-gpu-windows-x86_64-2.6.0" ^
+@REM gpu needs CUDA dlls
 cmake -G "Visual Studio 16 2019" -A x64 ^
 -Dtensorflow_ROOT="%DOWNLOAD_DIR_LINUX%/libtensorflow-cpu-windows-x86_64-2.6.0" ^
 -B./build
